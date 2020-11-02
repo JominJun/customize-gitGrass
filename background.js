3b272ece7c9da224ece309af60c30e1cc2e82b9b
Minjun <mj3029327@naver.com>
2020-11-02 11:54:10 +0900

    코드 정리

--- background.js
+++ background.js
@@ -1,63 +1,55 @@
 const halloween = ["#ebedf0", "#ffee4a", "#ffc501", "#fe9600", "#03001c"];
 
+const updateCommitColor = (colorList) => {
+  chrome.tabs.executeScript(
+    {
+      code:
+        `
+        rectFillList = [];
+        for(let i=0; i<365; i++){
+          rectFillList.push(document.getElementsByTagName('rect')[i].getAttribute('fill'))
+        }
+        rectFillList
+        `,
+    },
+    (results) => {
+      rectFillList = String(results).split(",");
+
+      for (let id in rectFillList) {
+        for(let i=1; i<=4; i++) {
+          if(rectFillList[id].includes(i)) {
+            chrome.tabs.executeScript({
+              code: `document.getElementsByTagName('rect')[${id}].style.fill = "${colorList[i]}"`,
+            });
+          }
+        }
+      }
+    }
+  )
+}
+
+const updateIndexColor = (colorList) => {
+  for(let i=0; i<5; i++) {
+    chrome.tabs.executeScript({
+      code: `document.querySelectorAll("ul.legend li")[${i}].style.background = "${colorList[i]}"`
+    });
+  }
+}
+
+const changeColor = (theme) => {
+  switch(theme) {
+    case "halloween":
+      updateIndexColor(halloween);
+      updateCommitColor(halloween);
+      break;
+  }
+}
+
 chrome.webNavigation.onCompleted.addListener(
   () => {
-    chrome.tabs.executeScript(
-      {
-        code:
-          "rectFillList = [];\
-          for(let i=0; i<365; i++){ rectFillList.push(document.getElementsByTagName('rect')[i].getAttribute('fill')) }\
-          rectFillList",
-      },
-      (results) => {
-        chrome.storage.local.get(["theme"], (res) => {
-          if (res.theme == "halloween") {
-            chrome.tabs.executeScript({
-              code: `
-                  document.querySelectorAll("ul.legend li")[0].style.background = "#ebedf0"
-                  document.querySelectorAll("ul.legend li")[1].style.background = "#ffee4a"
-                  document.querySelectorAll("ul.legend li")[2].style.background = "#ffc501"
-                  document.querySelectorAll("ul.legend li")[3].style.background = "#fe9600"
-                  document.querySelectorAll("ul.legend li")[4].style.background = "#03001c"
-                  `,
-            });
-
-            list = String(results).split(",");
-            for (let j in list) {
-              switch (list[j]) {
-                case "var(--color-calendar-graph-day-bg)":
-                  list[j] = "0";
-                  break;
-                case "var(--color-calendar-graph-day-L1-bg)":
-                  list[j] = "1";
-                  chrome.tabs.executeScript({
-                    code: `document.getElementsByTagName('rect')[${j}].style.fill = "#ffee4a"`,
-                  });
-                  break;
-                case "var(--color-calendar-graph-day-L2-bg)":
-                  list[j] = "2";
-                  chrome.tabs.executeScript({
-                    code: `document.getElementsByTagName('rect')[${j}].style.fill = "#ffc501"`,
-                  });
-                  break;
-                case "var(--color-calendar-graph-day-L3-bg)":
-                  list[j] = "3";
-                  chrome.tabs.executeScript({
-                    code: `document.getElementsByTagName('rect')[${j}].style.fill = "#fe9600"`,
-                  });
-                  break;
-                case "var(--color-calendar-graph-day-L4-bg)":
-                  list[j] = "4";
-                  chrome.tabs.executeScript({
-                    code: `document.getElementsByTagName('rect')[${j}].style.fill = "#03001c"`,
-                  });
-                  break;
-              }
-            }
-          }
-        });
-      }
-    );
+    chrome.storage.local.get(["theme"], (res) => {
+      changeColor(res.theme)
+    });
   },
   {
     url: [
--- settings.js
+++ settings.js
@@ -1,24 +1,31 @@
-originalBtn = document.querySelector("#original");
-halloweenBtn = document.querySelector("#halloween");
+const originalBtn = document.querySelector("#original");
+const halloweenBtn = document.querySelector("#halloween");
+
+const updateStorageInfo = (theme) => {
+  chrome.storage.local.set({ theme: theme });
+
+  chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+    chrome.tabs.executeScript({
+      code: `
+        if("${tabs[0].url}".includes("github")){
+          window.location.reload()
+        }`,
+    });
+    window.location.reload();
+  });
+}
+
+window.onload = () => {
+  chrome.storage.local.get(["theme"], (res) => {
+    if (!res.theme) now_theme = "original";
+    document.getElementById("theme_condition").textContent = res.theme;
+  });
+}
 
 originalBtn.addEventListener("click", () => {
-  chrome.storage.local.set({ theme: "original" });
-  chrome.tabs.executeScript({
-    code: `window.location.reload()`,
-  });
-  window.location.reload();
+  updateStorageInfo("original")
 });
 
 halloweenBtn.addEventListener("click", () => {
-  chrome.storage.local.set({ theme: "halloween" });
-  chrome.tabs.executeScript({
-    code: `window.location.reload()`,
-  });
-  window.location.reload();
-});
-
-var now_theme;
-chrome.storage.local.get(["theme"], (res) => {
-  now_theme = res.theme;
-  document.getElementById("theme_condition").textContent = now_theme;
+  updateStorageInfo("halloween")
 });
